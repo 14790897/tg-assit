409084fa9fb43160462068419234d2c6489ed23f
Liuweiqing <121866954+14790897@users.noreply.github.com>
2023-10-17 08:33:47 +0800

    'a'

--- tg/bot.js
+++ tg/bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const bot = new TelegramBot('6631000897:AAFmL1ZHyIKRMs0eou-vZy4gnP6RkUeb1ww', {polling: true});
+const bot = new TelegramBot('env-token', {polling: true});
 
 // Listen for any kind of message. There are different kinds of messages.
 bot.on('message', (msg) => {
